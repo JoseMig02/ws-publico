db3bc19d22fb82f079b01888c3906934e04ea7ab
Jose miguel <martineznene756@gmail.com>
2025-03-09 12:34:55 -0400

    feat: configured Swagger Module

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+// import { json, urlencoded } from 'express';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Some Configuration for API (Not about Swagger)
+  // app.use(json({ limit: '50mb' }));
+  // app.use(urlencoded({ extended: true, limit: '50mb' }));
+
+  const apiPath = 'api';
+  app.setGlobalPrefix(apiPath);
+
+  const options = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('WS Public Swagger API')
+    .setDescription('WS Public Swagger API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  // Swagger path: http://localhost:3200/api/docs
+  SwaggerModule.setup(`${apiPath}/docs`, app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
