518d56b684de915f82d0f16d2d47f1d4fd578bae
Jose miguel <martineznene756@gmail.com>
2025-04-15 12:07:14 -0400

    feat: searching functionality

--- src/logs/logs.controller.ts
+++ src/logs/logs.controller.ts
@@ -11,13 +11,15 @@ export class LogsController {
   @ApiQuery({ name: 'to', required: false, description: 'Fecha final del filtro' })
   @ApiQuery({ name: 'page', required: false, type: Number, description: 'Página de resultados', example: 1 })
   @ApiQuery({ name: 'limit', required: false, type: Number, description: 'Número de resultados por página', example: 10 })
+  @ApiQuery( { name: 'search',required: false,description: 'Filtro de búsqueda',example: 'credit', type: String})
   async getAllLogs(
     @Query('from') from: string, 
     @Query('to') to: string, 
     @Query('page') page: number = 1,   
-    @Query('limit') limit: number = 10
+    @Query('limit') limit: number = 10,
+    @Query('search') search : string = '',
   ) {
-    return await this.serviceLog.getLogs(from, to, page, limit);
+    return await this.serviceLog.getLogs(from, to, page, limit,search);
   }
   @Get('/stats')
   async getStats() {
